aaf25b47378eb583d5746ac2dcc0d5adb0941d27
Payal Binnod <54414304+payal2749@users.noreply.github.com>
2021-08-02 02:01:15 -0700

    initial commitof math.py

---
+++ math.py
@@ -0,0 +1,13 @@
+#add implementation
+def add(x,y):
+   pass
+def substract(x,y):
+   retrun x-y
+#multiply
+def multiply(x,y):
+   return x*y
+#bjhghh
+def divide(x,y):
+    retun x/y
+def square(x):
+    pass
